70039cbc3c0fbb9744127b2db7270fb53bf258d2
HikariWoosh <wps21vfu@uea.ac.uk>
2023-05-09 15:29:31 +0100

    Changed Fish
    Fish > Money

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {Route, Routes, BrowserRouter, Outlet} from 'react-router-dom';
 import './App.css';
-import Randompage from './Pages/randompage/Randompage.jsx';
+import Randompage from './Pages/salesRepView/salesRepView.jsx';
 import StaticDataContextProvider from './Contexts/StaticDataContext';
 import {ProductList} from './Widgets/widgets';
 
@@ -13,7 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Outlet></Outlet>}>
               <Route index element = {<ProductList></ProductList>}></Route>
-              <Route path="/fish" element={<Randompage></Randompage>}></Route>
+              <Route path="/salesRepView" element={<Randompage></Randompage>}></Route>
             </Route>            
         </Routes>
 			</BrowserRouter>
---
+++ Client/src/Pages/salesRepView/salesRepView.jsx
@@ -0,0 +1,4 @@
+import './salesRepView.css'
+export default function Randompage(){
+    return(<h1>Money</h1>);
+}
