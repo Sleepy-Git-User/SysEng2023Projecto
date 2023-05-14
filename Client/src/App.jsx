a0f246bcfdf0bc54c29ddf32ae5de8038db8cfc5
Callum <bvz20kvu@uea.ac.uk>
2023-05-14 19:59:09 +0100

    Added login page, ready to add database functionality

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,21 +2,40 @@ import { useState } from 'react'
 import {Route, Routes, BrowserRouter, Outlet} from 'react-router-dom';
 import './App.css';
 import Randompage from './Pages/randompage/Randompage.jsx';
+import Loginpage from './Pages/randompage/Loginpage.jsx';
 import StaticDataContextProvider from './Contexts/StaticDataContext';
 import {ProductList} from './Widgets/widgets';
+
+import {red} from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5883a7',
+    },
+    secondary: {
+      main: '#5883a7',
+    },
+  },
+});
+
 
 function App() {
 
   return (
     <StaticDataContextProvider>
-        <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <BrowserRouter>
           <Routes>
             <Route path="/" element={<Outlet></Outlet>}>
               <Route index element = {<ProductList></ProductList>}></Route>
               <Route path="/fish" element={<Randompage></Randompage>}></Route>
+              <Route path="/login" element={<Loginpage></Loginpage>}></Route>
             </Route>            
         </Routes>
 			</BrowserRouter>
+      </ThemeProvider>
     </StaticDataContextProvider>
     )
 }
