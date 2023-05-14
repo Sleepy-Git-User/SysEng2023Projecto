bb1447c89e6f40ec2e6aa05d1b96182716c6073e
HikariWoosh <wps21vfu@uea.ac.uk>
2023-05-14 04:12:56 +0100

    Added price feature, add feature (No Visuals), started discount

--- Client/src/Pages/salesRepView/salesRepView.jsx
+++ Client/src/Pages/salesRepView/salesRepView.jsx
@@ -1,41 +1,67 @@
 import './salesRepView.css'
-import React from "react"
+import React, { useState } from 'react';
 import { Container } from '@mui/material';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/material';
 import { Button } from '@mui/material';
 import { Backdrop } from '@mui/material';
 import { CircularProgress } from '@mui/material'
+import { List } from '@mui/material'
+import {Input} from '@mui/material'
 
 export default function Randompage(){
-    /* Function to change the open or close state of the backdrop */
+    /* Calculate total */
+    let zero = 0;
+    let runningtotal = zero.toFixed(2);
+
+    const [total, setTotal] = React.useState(runningtotal)
+    const shirtBrought = () => (setTotal((+total + 7.50).toFixed(2)))
+    const tshirtBrought = () => (setTotal((+total + 4.50).toFixed(2)))
+    const socksBrought = () => (setTotal((+total + 2.99).toFixed(2)))
+    const jeansBrought = () => (setTotal((+total + 21.00).toFixed(2)))
+    const clearTotal = () => (setTotal((+total - +total).toFixed(2)))
+
+    /* Calculate discounts given the proper input */
+    const [discountCode, setDiscount] = React.useState(runningtotal)
+    const discount  = () => (setTotal((+total * 0.8).toFixed(2)))
+
+    /* Change the open or close state of the backdrop */
     const [open, setOpen] = React.useState(false)
     const handleBackdrop = () => {setOpen(!open)}
 
-    /* Function to change the visibility of the box */
-    const [hiddenBox, setHideBox] = React.useState(false)
+    /* Change the visibility of the checkout box */
+    const [hiddenBox, setHideBox] = React.useState('hidden')
     const handleCheckBoxShow = () => (setHideBox('visible'))
     const handleCheckBoxHide = () => (setHideBox('hidden'))
 
-        /* Function to change the visibility of the box */
-        const [hiddenConfirm, setConfirmBox] = React.useState(false)
-        const handleConfirmShow = () => (setConfirmBox('visible'))
-        const handleConfirmHide = () => (setConfirmBox('hidden'))
+
+    /* Change the visibility of the confirm box */
+    const [hiddenConfirm, setConfirmBox] = React.useState('hidden')
+    const handleConfirmShow = () => (setConfirmBox('visible'))
+    const handleConfirmHide = () => (setConfirmBox('hidden'))
+
+    /* Change the visibility of the add box */
+    const [hiddenAdd, setAddBox] = React.useState('hidden')
+    const handleAddShow = () => (setAddBox('visible'))
+    const handleAddHide = () => (setAddBox('hidden'))
+
+        /* Change the visibility of the add box */
+        const [hiddenDiscount, setDiscountBox] = React.useState('hidden')
+        const handleDiscountShow = () => (setDiscountBox('visible'))
+        const handleDiscountHide = () => (setDiscountBox('hidden'))
 
     return(
         <Container>
             {/* List of buttons */}
             <Button sx={{m:2, top: 65, left: 845, minWidth: 150, minHeight: 75}} variant="contained" onClick={handleBackdrop}>Logout</Button>
-            <Button sx={{m:2, top: 970, right: 200, minWidth: 150, minHeight: 75}} variant="contained" onClick={handleBackdrop} color="success">Add</Button>
+            <Button sx={{m:2, top: 970, right: 200, minWidth: 150, minHeight: 75}} variant="contained" onClick={() =>{handleBackdrop(); handleAddShow();}} color="success">Add</Button>
             <Button sx={{m:2, top: 970, right: 100, minWidth: 150, minHeight: 75}} variant="contained" onClick={handleBackdrop} color="error">Remove</Button>
-            <Button sx={{m:2, top: 970, left: 300, minWidth: 150, minHeight: 75 }} variant="contained" onClick={() =>{handleBackdrop(); handleCheckBoxShow();}} color="warning" >Checkout</Button>
+            <Button sx={{m:2, top: 970, left: 0, minWidth: 150, minHeight: 75}} variant="contained" onClick={() =>{handleBackdrop(); discount(); handleDiscountShow();}} color="secondary">Discount</Button>
+            <Button sx={{m:2, top: 970, left: 100, minWidth: 150, minHeight: 75 }} variant="contained" onClick={() =>{handleBackdrop(); handleCheckBoxShow();}} color="warning" >Checkout</Button>
 
             {/* Username of the user will be shown here */}
             <Typography sx={{color: '#5883a7'}} variant="h3">Username</Typography>
             
-
-
-        
             {/* Display Box */}
              <Box display="flex" justifyContent="center" alignItems="center" variant="outlined" sx={{ 
                 zIndex: 0,
@@ -56,8 +82,24 @@ export default function Randompage(){
                     >
                     {/* Buttons to pay with either cash or card */}
                     <Typography sx={{height: 100, width:600}} display="flex" justifyContent="center" alignItems="center" variant="outlined" >How would the customer like to pay</Typography>
-                    <Button  sx={{height: 400, width:300,}} variant="outlined" onClick={() =>{handleCheckBoxHide(); handleConfirmShow();}}>Cash</Button>
-                    <Button  sx={{height: 400, width:300,}} variant="outlined" onClick={() =>{handleCheckBoxHide(); handleConfirmShow();}}>Card</Button>
+                    <Button  sx={{height: 400, width:300,}} variant="outlined" onClick={() =>{handleCheckBoxHide(); handleConfirmShow(); clearTotal();}}>Cash</Button>
+                    <Button  sx={{height: 400, width:300,}} variant="outlined" onClick={() =>{handleCheckBoxHide(); handleConfirmShow(); clearTotal();}}>Card</Button>
+                </Box>
+
+                {/* Buying Box */}
+                <Box visibility={hiddenAdd} position={'absolute'} sx={{ 
+                    zIndex: 1100,
+                    border: 5 ,
+                    width: 800, 
+                    height: 700, 
+                    bgcolor: "white"}} 
+                    >
+                        <List sx={{maxHeight: 682, overflow: 'auto'}}>
+                        <Button sx={{height: 200, width:775}} varient="outlined" onClick={jeansBrought}>Add Jeans</Button>
+                        <Button sx={{height: 200, width:775}} varient="outlined" onClick={shirtBrought}>Add Shirt</Button>
+                        <Button sx={{height: 200, width:775}} varient="outlined" onClick={tshirtBrought}>Add T-Shirt</Button>
+                        <Button sx={{height: 200, width:775}} varient="outlined" onClick={socksBrought}>Add Socks</Button>
+                        </List>
                 </Box>
 
                 {/* Confirmation Box */}
@@ -69,12 +111,45 @@ export default function Randompage(){
                     bgcolor: "white"}} 
                     >
                     <Typography variant = "h4">Payment Succesful</Typography>
+                    
+                </Box>
+
+                    {/* Discount Box */}
+                <Box visibility={hiddenDiscount} position={'absolute'} display="flex" justifyContent="center" alignItems="center" variant="outlined" sx={{ 
+                    zIndex: 1200,
+                    border: 5 ,
+                    width: 300, 
+                    height: 200, 
+                    bgcolor: "white"}} 
+                    >
+                    <Input
+                    name="Code"
+                    placeholder="Discount Code"
+                    defaultValue = ''
+                    required
+                    size="large"
+                    />
+                                        
+                </Box>
+
+                {/* Price */}
+                <Box position={'absolute'} display="flex" justifyContent="right" alignItems="right" variant="outlined" sx={{ 
+                    zIndex: 0,
+                    border: 2 ,
+                    top: 890,
+                    width: 1000, 
+                    height: 75, 
+                    bgcolor: "white"}} 
+                    >
+                    <text>{'Price: £' + total}</text>
                 </Box>
             </Box>
 
                 {/* Backdrop that shows loading when a button is pressed. */}
-                <Backdrop open={open} onClick={() =>{handleBackdrop(); handleConfirmHide(); handleCheckBoxHide();}} sx={{zIndex: 100, position: "absolute"}}>
-                    <CircularProgress></CircularProgress>
+                <Backdrop open={open} onClick={() =>{handleBackdrop(); handleConfirmHide(); handleCheckBoxHide(); handleAddHide(); handleDiscountHide();}} sx={{zIndex: 100, position: "absolute"}}>
+                    <CircularProgress>
+
+                    </CircularProgress>
                 </Backdrop>
         </Container>
     )
